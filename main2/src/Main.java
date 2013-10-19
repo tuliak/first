948a8d53013513e8a1e7bacc55c01db2624b14cf
tuliak <grc.tula@gmail.com>
2013-10-19 22:30:51 +0400

    initial project version

---
+++ main2/src/Main.java
@@ -0,0 +1,16 @@
+import com.lesson.Human;
+
+public class Main {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setName("Andrew");
+        System.out.println(human.getName());
+        human.setAge(-12);
+        System.out.println(human.getAge());
+        System.out.println(human.getAge());
+        human.setName(null);
+        System.out.println(human.getName());
+        human.setName("A");
+        System.out.println(human.getName());
+    }
+}
